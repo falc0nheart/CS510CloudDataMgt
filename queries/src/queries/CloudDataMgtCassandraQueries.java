7ae984c9b40a13caeaf2bf1b4171694cba67c14d
Jim Miller <jgm2@pdx.edu>
2014-03-15 10:24:44 -0700

    Added some example code from https://github.com/hector-client/hector/blob/master/core/src/main/java/me/prettyprint/cassandra/examples/ExampleDaoV2.java

--- queries/src/queries/CloudDataMgtCassandraQueries.java
+++ queries/src/queries/CloudDataMgtCassandraQueries.java
@@ -2,6 +2,14 @@ package queries;
 
 import java.util.List;
 
+import static me.prettyprint.hector.api.factory.HFactory.createColumn;
+import static me.prettyprint.hector.api.factory.HFactory.createColumnQuery;
+import static me.prettyprint.hector.api.factory.HFactory.createKeyspace;
+import static me.prettyprint.hector.api.factory.HFactory.createMultigetSliceQuery;
+import static me.prettyprint.hector.api.factory.HFactory.createMutator;
+import static me.prettyprint.hector.api.factory.HFactory.getOrCreateCluster;
+
+import me.prettyprint.cassandra.serializers.StringSerializer;
 import me.prettyprint.cassandra.service.template.ColumnFamilyTemplate;
 import me.prettyprint.hector.api.Cluster;
 import me.prettyprint.hector.api.Keyspace;
@@ -9,40 +17,76 @@ import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
 import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
 import me.prettyprint.hector.api.exceptions.HectorException;
 import me.prettyprint.hector.api.factory.HFactory;
+import me.prettyprint.hector.api.Serializer;
+import me.prettyprint.hector.api.mutation.Mutator;
+import me.prettyprint.hector.api.query.ColumnQuery;
+import me.prettyprint.hector.api.query.MultigetSliceQuery;
+import me.prettyprint.hector.api.query.QueryResult;
+import me.prettyprint.hector.api.beans.HColumn;
+import me.prettyprint.hector.api.beans.Rows;
 
 public class CloudDataMgtCassandraQueries {
+	
+  private final static String KEYSPACE = "CloudDataMgt";
+  private final static String HOST_PORT = "54.186.36.251:9160";
+  private final static String CF_NAME = "Stations";
+  /** Column name where values are stored */
+  private final static String COLUMN_NAME = "LocationText";
+  private final StringSerializer serializer = StringSerializer.get();
+  private Keyspace keyspace;
 
-	public static void main(String[] args) {
-		Cluster myCluster = HFactory.getOrCreateCluster("CloudDataMgtCluster","54.186.36.251:9160");
-		System.out.println("cluster name:  " + myCluster.describeClusterName());
-		
-		KeyspaceDefinition kspDef = myCluster.describeKeyspace("sandboxalex");
-		String kspName = kspDef.getName();
-		System.out.println("keyspace name:  " + kspName);
-		
-		//Keyspace ksp = HFactory.createKeyspace("CloudDataMgt", myCluster);
-		//System.out.println(ksp.getKeyspaceName());
-		
-		
-		List<ColumnFamilyDefinition> lsCf = kspDef.getCfDefs();
-		System.out.println("# of column family names:  " + lsCf.size());
-		System.out.print("column family names:  ");
-		for (int i = 0; i < lsCf.size(); i++) {
-			System.out.print(lsCf.get(i).getName() + "  ");
-		}
-		
-//		ColumnFamilyTemplate<String, String> template =
-//        new ThriftColumnFamilyTemplate<String, String>(ksp, ,
-//                                                       StringSerializer.get(),
-//                                                       StringSerializer.get());
+  public static void main(String[] args) throws HectorException {
+    Cluster c = HFactory.getOrCreateCluster("MyCluster", HOST_PORT);
+    CloudDataMgtCassandraQueries ed = new CloudDataMgtCassandraQueries(HFactory.createKeyspace(KEYSPACE, c));
+    //ed.insert("key1", "value1", StringSerializer.get());
+
+    System.out.println(ed.get("1048", StringSerializer.get()));
+  }
+  
+  public CloudDataMgtCassandraQueries(Keyspace keyspace) {
+    this.keyspace = keyspace;
+  }
+  
+  public <K> String get(final K key, Serializer<K> keySerializer) throws HectorException {
+    ColumnQuery<K, String, String> q = createColumnQuery(keyspace, keySerializer, serializer, serializer);
+    QueryResult<HColumn<String, String>> r = q.setKey(key).
+        setName(COLUMN_NAME).
+        setColumnFamily(CF_NAME).
+        execute();
+    HColumn<String, String> c = r.get();
+    return c == null ? null : c.getValue();
+  }
+	  
+//		Cluster myCluster = HFactory.getOrCreateCluster("CloudDataMgtCluster","54.186.36.251:9160");
+//		System.out.println("cluster name:  " + myCluster.describeClusterName());
 //		
-//		try {
-//	    ColumnFamilyResult<String, String> res = template.queryColumns("a key");
-//	    String value = res.getString("domain");
-//	    // value should be "www.datastax.com" as per our previous insertion.
-//		} catch (HectorException e) {
-//	    // do something ...
+//		KeyspaceDefinition kspDef = myCluster.describeKeyspace("sandboxalex");
+//		String kspName = kspDef.getName();
+//		System.out.println("keyspace name:  " + kspName);
+//		
+//		//Keyspace ksp = HFactory.createKeyspace("CloudDataMgt", myCluster);
+//		//System.out.println(ksp.getKeyspaceName());
+//		
+//		
+//		List<ColumnFamilyDefinition> lsCf = kspDef.getCfDefs();
+//		System.out.println("# of column family names:  " + lsCf.size());
+//		System.out.print("column family names:  ");
+//		for (int i = 0; i < lsCf.size(); i++) {
+//			System.out.print(lsCf.get(i).getName() + "  ");
 //		}
-	}
+//		
+////		ColumnFamilyTemplate<String, String> template =
+////        new ThriftColumnFamilyTemplate<String, String>(ksp, ,
+////                                                       StringSerializer.get(),
+////                                                       StringSerializer.get());
+////		
+////		try {
+////	    ColumnFamilyResult<String, String> res = template.queryColumns("a key");
+////	    String value = res.getString("domain");
+////	    // value should be "www.datastax.com" as per our previous insertion.
+////		} catch (HectorException e) {
+////	    // do something ...
+////		}
 
 }
+
