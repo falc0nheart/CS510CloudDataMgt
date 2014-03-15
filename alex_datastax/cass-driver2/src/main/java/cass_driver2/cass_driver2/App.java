125ea00dcb02db3a778f0c5d95afe76b0823ec43
Alex Laughnan <laughnan@pdx.edu>
2014-03-15 11:36:54 -0700

    Testing on SELECT statement for CQL3 from books.

--- alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/App.java
+++ alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/App.java
@@ -15,16 +15,18 @@ public class App
                 .addContactPoints("54.186.36.251")
                 .build();
     	Session session = cluster.connect();
-    	String cqlStatement = "CREATE KEYSPACE myfirstcassandradb WITH " + 
-    			"replication = {'class':'SimpleStrategy','replication_factor':3}";        
-    	session.execute(cqlStatement);
-
-    	String cqlStatement2 = "CREATE TABLE myfirstcassandradb.users (" + 
-    			" user_name varchar PRIMARY KEY," + 
-    			" password varchar " + 
-    			");";
-    	session.execute(cqlStatement2);
-
+//    	String cqlStatement = "CREATE KEYSPACE myfirstcassandradb WITH " + 
+//    			"replication = {'class':'SimpleStrategy','replication_factor':3}";        
+//    	session.execute(cqlStatement);
+//
+//    	String cqlStatement2 = "CREATE TABLE myfirstcassandradb.users (" + 
+//    			" user_name varchar PRIMARY KEY," + 
+//    			" password varchar " + 
+//    			");";
+//    	session.execute(cqlStatement2);
+    	
+    	String cqlStatement3 = "SELECT * FROM sandboxalex.books;";
+    	System.out.println(session.execute(cqlStatement3));
     	System.out.println("Done");
     	System.exit(0);
     }
