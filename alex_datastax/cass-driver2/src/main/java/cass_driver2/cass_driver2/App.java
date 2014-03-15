5eb7f6c23530b7b95945745810486d82ff056f5c
Alex Laughnan <laughnan@pdx.edu>
2014-03-15 11:25:36 -0700

    Added some test code for datastax/java-driver.

---
+++ alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/App.java
@@ -0,0 +1,31 @@
+package cass_driver2.cass_driver2;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+/**
+ * Hector sucks!
+ * But maybe DataStax is better!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Cluster cluster = Cluster.builder()
+                .addContactPoints("54.186.36.251")
+                .build();
+    	Session session = cluster.connect();
+    	String cqlStatement = "CREATE KEYSPACE myfirstcassandradb WITH " + 
+    			"replication = {'class':'SimpleStrategy','replication_factor':3}";        
+    	session.execute(cqlStatement);
+
+    	String cqlStatement2 = "CREATE TABLE myfirstcassandradb.users (" + 
+    			" user_name varchar PRIMARY KEY," + 
+    			" password varchar " + 
+    			");";
+    	session.execute(cqlStatement2);
+
+    	System.out.println("Done");
+    	System.exit(0);
+    }
+}
