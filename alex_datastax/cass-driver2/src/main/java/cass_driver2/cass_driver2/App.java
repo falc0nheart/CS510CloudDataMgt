0d5ada513bfd70a2b8b8e158ce420a57a2bdeb5d
Alex Laughnan <laughnan@pdx.edu>
2014-03-15 15:14:49 -0700

    Updated with documentation and code clean-up.

--- alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/App.java
+++ alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/App.java
@@ -14,18 +14,7 @@ public class App
     	Cluster cluster = Cluster.builder()
                 .addContactPoints("54.186.36.251")
                 .build();
-    	Session session = cluster.connect();
-//    	String cqlStatement = "CREATE KEYSPACE myfirstcassandradb WITH " + 
-//    			"replication = {'class':'SimpleStrategy','replication_factor':3}";        
-//    	session.execute(cqlStatement);
-//
-//    	String cqlStatement2 = "CREATE TABLE myfirstcassandradb.users (" + 
-//    			" user_name varchar PRIMARY KEY," + 
-//    			" password varchar " + 
-//    			");";
-//    	session.execute(cqlStatement2);
-   
-    	
+    	Session session = cluster.connect();    	
     	String cqlStatement3 = "SELECT * FROM \"CloudDataMgt\".\"Stations\" LIMIT 5;";
     	System.out.println(session.execute(cqlStatement3).one());
     	System.out.println("Done");
