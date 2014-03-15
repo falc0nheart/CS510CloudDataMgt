e81254b5f5bd7d10c81adf7d789722d0237d7955
Jim Miller <jgm2@pdx.edu>
2014-03-15 00:03:55 -0700

    Debugging grabbing column families via Hector - see email

--- queries/src/queries/CloudDataMgtCassandraQueries.java
+++ queries/src/queries/CloudDataMgtCassandraQueries.java
@@ -1,7 +1,11 @@
 package queries;
 
+import java.util.List;
+
+import me.prettyprint.cassandra.service.template.ColumnFamilyTemplate;
 import me.prettyprint.hector.api.Cluster;
 import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
 import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
 import me.prettyprint.hector.api.exceptions.HectorException;
 import me.prettyprint.hector.api.factory.HFactory;
@@ -9,13 +13,29 @@ import me.prettyprint.hector.api.factory.HFactory;
 public class CloudDataMgtCassandraQueries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Cluster cluster = HFactory.createCluster("TestCluster",  new CassandraHostConfigurator("localhost:9160"));
 		Cluster myCluster = HFactory.getOrCreateCluster("CloudDataMgtCluster","54.186.36.251:9160");
-//		KeyspaceDefinition keyspaceDef = myCluster.describeKeyspace("CloudDataMgt");
-		Keyspace ksp = HFactory.createKeyspace("CloudDataMgt", myCluster);
-		System.out.println(myCluster.describeClusterName());
+		System.out.println("cluster name:  " + myCluster.describeClusterName());
 		
+		KeyspaceDefinition kspDef = myCluster.describeKeyspace("sandboxalex");
+		String kspName = kspDef.getName();
+		System.out.println("keyspace name:  " + kspName);
+		
+		//Keyspace ksp = HFactory.createKeyspace("CloudDataMgt", myCluster);
+		//System.out.println(ksp.getKeyspaceName());
+		
+		
+		List<ColumnFamilyDefinition> lsCf = kspDef.getCfDefs();
+		System.out.println("# of column family names:  " + lsCf.size());
+		System.out.print("column family names:  ");
+		for (int i = 0; i < lsCf.size(); i++) {
+			System.out.print(lsCf.get(i).getName() + "  ");
+		}
+		
+//		ColumnFamilyTemplate<String, String> template =
+//        new ThriftColumnFamilyTemplate<String, String>(ksp, ,
+//                                                       StringSerializer.get(),
+//                                                       StringSerializer.get());
+//		
 //		try {
 //	    ColumnFamilyResult<String, String> res = template.queryColumns("a key");
 //	    String value = res.getString("domain");
