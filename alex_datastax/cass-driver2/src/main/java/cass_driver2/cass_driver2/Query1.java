cbddf28c2a84e411ba667097f563cf95323c6764
Alex Laughnan <laughnan@pdx.edu>
2014-03-15 18:14:26 -0700

    Updated Query1.java

--- alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/Query1.java
+++ alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/Query1.java
@@ -7,30 +7,42 @@ import com.datastax.driver.core.*;
 import java.util.*;
 
 public class Query1 {
-	
-	// Find travel time for each NB station
-	// 5-minute intervals
-	// limited to 9/22/2011
-	
-	public static void main(String[] args) {
-    	Cluster cluster = Cluster.builder()
+
+
+
+
+    public static void main(String[] args) {
+        Cluster cluster = Cluster.builder()
                 .addContactPoints("54.186.36.251")
                 .build();
-    	Session session = cluster.connect();    	
-    	ArrayList stationList = getNBStations(session);    	
-    	System.out.println("Done");
-    	System.exit(0);
-	}
-	
-	public static ArrayList getNBStations (Session session) {   	
-    	// Query Stations
-    	String cqlQueryStationNB = "SELECT \"StationID\" FROM \"CloudDataMgt\".\"Stations\" WHERE \"ShortDirection\" = 'N' LIMIT 300;";
-    	ResultSet results = session.execute(cqlQueryStationNB);
-    	ArrayList returnedVal = new ArrayList();
-    	for(Row row: results) {
-    		returnedVal.add(row.getInt(0));
-    	}
-    	return returnedVal;
-	}
+        Session session = cluster.connect();
+        // Find travel time for each NB station
+        ResultSet stationList = getNBStations(session);
+        for (Row row : stationList) {
+            // More queries
+            int castedStation = row.getInt(0);
+            String cqlQuery = "SELECT \"StartHour\", \"StartMinute\", \"Speed\" FROM \"CloudDataMgt\".\"LoopData\" WHERE \"StationID\" = "
+            + castedStation + " AND \"StartDate\" > '2011-09-22 00:00' AND \"StartDate\" < '2011-09-23 23:59' LIMIT 300;";
+            ResultSet results = session.execute(cqlQuery);
+            System.out.println(results.all().size());
+            // 5-minute intervals
 
+            // put that here.
+        } // for
+        System.out.println("Query 1 - Done");
+        System.exit(0);
+    }
+
+    /*
+     * method: getNBStations
+     * argument: Session session - current Cassandra session
+     *
+     * This method will return all northbound stations and their
+     * LengthMid in the form of a ResultSet
+     */
+    public static ResultSet getNBStations (Session session) {
+        // Query Stations
+        String cqlQueryStationNB = "SELECT \"StationID\", \"LengthMid\" FROM \"CloudDataMgt\".\"Stations\" WHERE \"ShortDirection\" = 'N' LIMIT 300;";
+        return session.execute(cqlQueryStationNB);
+    } // getNBStations
 }
