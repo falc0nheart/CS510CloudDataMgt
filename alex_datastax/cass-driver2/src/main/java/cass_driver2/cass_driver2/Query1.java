a68f8b596015c10736981e8342fda7f54004b1a5
Alex Laughnan <laughnan@pdx.edu>
2014-03-15 19:19:53 -0700

    Too much to say.

--- alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/Query1.java
+++ alex_datastax/cass-driver2/src/main/java/cass_driver2/cass_driver2/Query1.java
@@ -1,15 +1,9 @@
 package cass_driver2.cass_driver2;
 
-//import com.datastax.driver.core.Cluster;
-//import com.datastax.driver.core.Session;
 import com.datastax.driver.core.*;
-
 import java.util.*;
 
 public class Query1 {
-
-
-
 
     public static void main(String[] args) {
         Cluster cluster = Cluster.builder()
@@ -18,16 +12,42 @@ public class Query1 {
         Session session = cluster.connect();
         // Find travel time for each NB station
         ResultSet stationList = getNBStations(session);
+        // Integer = 5-minute interval
+        // Float - Average Speed
+        HashMap<Integer, Float> newThing = new HashMap<Integer, Float>();
         for (Row row : stationList) {
             // More queries
             int castedStation = row.getInt(0);
             String cqlQuery = "SELECT \"StartHour\", \"StartMinute\", \"Speed\" FROM \"CloudDataMgt\".\"LoopData\" WHERE \"StationID\" = "
             + castedStation + " AND \"StartDate\" > '2011-09-22 00:00' AND \"StartDate\" < '2011-09-23 23:59' LIMIT 300;";
             ResultSet results = session.execute(cqlQuery);
-            System.out.println(results.all().size());
             // 5-minute intervals
-
-            // put that here.
+            HashMap<Integer, List<Integer>> intervalMap = new HashMap<Integer, List<Integer>>();
+            for(int i = 0; i < 287; i++) {
+                intervalMap.put(i, new ArrayList<Integer>());
+            } // for - initialize the List<Integer>
+            for(Row row2 : results) {
+                // Mathematical computation - assigns interval value between 0 - 287
+                int fiveMinInterval = (row2.getInt(0) * 12) + row2.getInt(1)/ 5;
+                // put Speed into HashMap based on 5-minute interval
+                List<Integer> waitwait = intervalMap.get(fiveMinInterval);
+                waitwait.add(row2.getInt(2));
+                intervalMap.put(fiveMinInterval, waitwait);
+            } // for
+            // for loop - getting average of intervalMap
+            for(int keyNotAList : intervalMap.keySet()) {
+                List<Integer> waitwait2 = intervalMap.get(keyNotAList);
+                int speedSum = 0;
+                for (int i = 0; i < waitwait2.size(); i++) {
+                    speedSum += waitwait2.get(i);
+                }
+                newThing.put(keyNotAList, (float) speedSum / waitwait2.size());
+            } // for
+            // Actually do average, cuz we lied before
+            for(int keyValue : newThing.keySet()){
+                float travelTime = (float) row.getDouble(1) / newThing.get(keyValue);
+                System.out.println ("Station: " + row.getInt(0) + " TRAVEL: " + travelTime);
+            } // for
         } // for
         System.out.println("Query 1 - Done");
         System.exit(0);
